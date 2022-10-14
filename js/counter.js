d6bc19e75b7d5dc418926a423db677bb85009a63
d3040 <daniel.clavijo@pm.me>
2022-10-13 22:23:02 -0500

    colors 3040

--- js/counter.js
+++ js/counter.js
@@ -1,12 +1,31 @@
 var deadline = new Date("12/16/22 09:00").getTime();
 
 setInterval(function() {
+  var match = 0;
   var now = new Date().getTime();
   var t = deadline - now;
+  //get days
   var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  //get hours
   var hours = Math.floor((t % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+  // get minutes
   var minutes = Math.floor((t % (1000 * 60 * 60 )) / (1000 * 60));
+  if (minutes == 30) {
+      match = match + 1;
+  };
+  // get seconds
   var seconds = Math.floor((t % (1000 * 60)) / 1000);
+  if (seconds == 40) {
+      match = match + 1;
+  };
 
-  document.getElementsByTagName("p")[0].innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+  document.getElementsByTagName("p")[0].innerHTML = days + "d " + hours + ":" + minutes + ":" + seconds;
+
+  // verify 3040
+  if (match == 2) {
+      document.getElementsByTagName("body")[0].className = "d3040";
+  } else {
+      document.getElementsByTagName("body")[0].className = "";
+  };
+
 }, 1000);
